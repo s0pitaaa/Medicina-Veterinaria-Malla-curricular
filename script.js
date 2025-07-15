7f7ffa03193c18b27973f91052e152a8f5a3e68b
s0pitaaa <luna.herrera.tapia@gmail.com>
2025-07-15 19:32:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
-<div id="contenedor"></div>
-
-  <script>
-    const malla = {
-      "1° Año": {
-        "1° Semestre": [
-          {codigo: "BIOL034", nombre: "Biología Celular", creditos: 8, prerequisitos: ""},
-          {codigo: "BIOL035", nombre: "Lab. Biocelular", creditos: 5, prerequisitos: ""},
-          {codigo: "DEBD221", nombre: "Zoología", creditos: 6, prerequisitos: ""},
-          {codigo: "FMMP003", nombre: "Matemática", creditos: 7, prerequisitos: ""},
-          {codigo: "MVET611", nombre: "Introducción a la Medicina Veterinaria", creditos: 4, prerequisitos: ""},
-          {codigo: "QUI002", nombre: "Química", creditos: 8, prerequisitos: ""},
-        ],
-        "2° Semestre": [
-          {codigo: "BIOL166", nombre: "Bioquímica", creditos: 8, prerequisitos: "Biol. Celular, Lab. Biocel., Química"},
-          {codigo: "CEGHC11", nombre: "Habilidades Comunicativas", creditos: 8, prerequisitos: ""},
-          {codigo: "ING119", nombre: "Inglés I", creditos: 12, prerequisitos: ""},
-          {codigo: "MVET621", nombre: "ADO I", creditos: 18, prerequisitos: "Biol. Celular, Lab. Biocel."},
-          {codigo: "MVET622", nombre: "Cuerpo Animal I", creditos: 19, prerequisitos: "Biol. Celular, Lab. Biocel., Zoología"},
-        ]
-      },
-      "2° Año": {
-        "3° Semestre": [
-          {codigo: "DEBD130", nombre: "Métodos Cuantitativos RR.NN", creditos: 9, prerequisitos: "Matemática"},
-          {codigo: "ING129", nombre: "Inglés II", creditos: 12, prerequisitos: "Inglés I"},
-          {codigo: "MVET631", nombre: "ADO II", creditos: 15, prerequisitos: "Bioquímica, ADO I"},
+const malla = {
           {codigo: "MVET632", nombre: "Cuerpo Animal II", creditos: 14, prerequisitos: "Cuerpo Animal I"},
           {codigo: "MVET633", nombre: "FDO I", creditos: 15, prerequisitos: "Cuerpo Animal I"},
         ],
@@ -32,4 +7,55 @@
           {codigo: "ING239", nombre: "Inglés III", creditos: 12, prerequisitos: "Inglés II"},
           {codigo: "MVET178", nombre: "Genética", creditos: 6, prerequisitos: "Métodos Cuantitativos, Bioquímica"},
           {codigo: "MVET240", nombre: "Anatomía Clínica", creditos: 13, prerequisitos: "Cuerpo Animal II, FDO I"},
+          {codigo: "MVET641", nombre: "FDO II", creditos: 24, prerequisitos: "FDO I, ADO II"},
         ]
+      },
+      "3° Año": {
+        "5° Semestre": [
+          {codigo: "DEBD160", nombre: "Biología de la Conservación", creditos: 8, prerequisitos: "Ecología General"},
+          {codigo: "ING249", nombre: "Inglés IV", creditos: 12, prerequisitos: "Inglés III"},
+          {codigo: "MVET192", nombre: "Farmacología", creditos: 12, prerequisitos: "FDO II"},
+          {codigo: "MVET340", nombre: "Enfermedades de Org. Acuáticos", creditos: 7, prerequisitos: "ADO II"},
+          {codigo: "MVET651", nombre: "Anatomía Patológica", creditos: 18, prerequisitos: "Cuerpo Animal II, FDO II"},
+          {codigo: "MVET652", nombre: "Nutrición y Alimentación Animal", creditos: 9, prerequisitos: "Genética"}
+        ],
+        "6° Semestre": [
+          {codigo: "CEGCT12", nombre: "Razonamiento Científico", creditos: 6, prerequisitos: "Habilidades Comunicativas"},
+          {codigo: "MVET230", nombre: "Patología Clínica", creditos: 21, prerequisitos: "Anatomía Clínica, FDO II"},
+          {codigo: "MVET310", nombre: "Imagenología", creditos: 14, prerequisitos: "Anatomía Patológica"},
+          {codigo: "MVET440", nombre: "Epidemiología y Salud Pública", creditos: 13, prerequisitos: "Métodos Cuantitativos, FDO II"},
+          {codigo: "MVET661", nombre: "Reproducción", creditos: 12, prerequisitos: "FDO I, Cuerpo Animal II"}
+        ]
+      },
+      "4° Año": {
+        "7° Semestre": [
+          {codigo: "DEBD161", nombre: "Manejo de Fauna Silvestre", creditos: 6, prerequisitos: "Biología de la Conservación, FDO I"},
+          {codigo: "IAMB662", nombre: "Legislación y Evaluación Ambiental", creditos: 9, prerequisitos: "Ecología, Biología de la Conservación"},
+          {codigo: "MVET671", nombre: "Inocuidad de Alimentos", creditos: 10, prerequisitos: "Epidemiología y Salud Pública"},
+          {codigo: "MVET673", nombre: "Medicina", creditos: 25, prerequisitos: "FDO II, Anatomía Clínica, Farmacología"},
+          {codigo: "MVET674", nombre: "Sistemas de Producción Animal", creditos: 16, prerequisitos: "Nutrición y Alimentación, Reproducción, Enfermedades Acuáticos"}
+        ],
+        "8° Semestre": [
+          {codigo: "DEBD180", nombre: "Formulación Proyectos RRNN", creditos: 8, prerequisitos: "Legislación Ambiental"},
+          {codigo: "MVET280", nombre: "Cirugía", creditos: 18, prerequisitos: "Medicina, Imagenología"},
+          {codigo: "MVET370", nombre: "Zoonosis y Enf. Emergentes", creditos: 8, prerequisitos: "Inocuidad de Alimentos"},
+          {codigo: "MVET681", nombre: "Patobiología Molecular", creditos: 15, prerequisitos: "Patología Clínica"},
+          {codigo: "MVET682", nombre: "Práctica Profesional", creditos: 16, prerequisitos: "Imagenología, Medicina"}
+        ]
+      },
+      "5° Año": {
+        "9° Semestre": [
+          {codigo: "CEGPC13", nombre: "Pensamiento Crítico", creditos: 5, prerequisitos: "Razonamiento Científico, Habilidades Comunicativas"},
+          {codigo: "MVET691", nombre: "Ética y Bienestar Animal", creditos: 7, prerequisitos: "Sistemas de Producción Animal, Manejo de Fauna"},
+          {codigo: "MVET692", nombre: "Innovación y Transferencia Tecnológica", creditos: 7, prerequisitos: "Formulación Proyectos RRNN"},
+          {codigo: "MVET693", nombre: "Clínica", creditos: 39, prerequisitos: "Cirugía, Patología Clínica"},
+          {codigo: "MVET694", nombre: "Proyecto de Título", creditos: 13, prerequisitos: "Zoonosis, Cirugía, Patobiología Molecular, Evaluación Proyectos RRNN, Práctica Profesional"}
+        ],
+        "10° Semestre": [
+          {codigo: "CEGRS14", nombre: "Responsabilidad Social", creditos: 6, prerequisitos: "Pensamiento Crítico"},
+          {codigo: "MVET695", nombre: "Electivo Profesional I", creditos: 15, prerequisitos: "Zoonosis, Patobiología Molecular, Evaluación Proyectos RRNN, Práctica Profesional, Clínica"},
+          {codigo: "MVET696", nombre: "Electivo Profesional II", creditos: 15, prerequisitos: "Zoonosis, Cirugía, Patobiología Molecular, Evaluación Proyectos RRNN, Práctica Profesional"},
+          {codigo: "MVET697", nombre: "Internado", creditos: 22, prerequisitos: "Clínica, Ética y Bienestar Animal"}
+        ]
+      }
+    };
